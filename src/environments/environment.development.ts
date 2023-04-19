366f4fc1c62f5342bfa249e84cfa243b522c727f
MariaeduardaRomancini <me2355732@gmail.com>
2023-04-19 15:51:35 -0300

    AULA PROFESSOR CLOVIS
    projeto Kanban fire

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'kanbanfire-be3d5',
+    appId: '1:111062721192:web:dc56500d50322756ae189d',
+    storageBucket: 'kanbanfire-be3d5.appspot.com',
+    apiKey: 'AIzaSyAw91n-bWgTExMMLoxEvNE7h_8_LumGoHM',
+    authDomain: 'kanbanfire-be3d5.firebaseapp.com',
+    messagingSenderId: '111062721192',
+    measurementId: 'G-QVNRCGEWG5',
+  },};
